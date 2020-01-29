ee4995f8f69f0e82f31f95d4ab0d391b5acff4d0
Anders Juul <andersjuulsfirma@gmail.com>
2020-01-29 15:32:03 +0100

    a

---
+++ Ajf.NsPlanner.Setup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ajf.NsPlanner.Setup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
